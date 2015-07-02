03eee9a01db20f47e0012cadf1a35afdbf38f6a5
jbengoa <juan.bengoa@gmail.com>
2015-07-02 10:49:17 -0400

    Se hicieron algunas modificaciones

--- scripts/services/crud.js
+++ scripts/services/crud.js
@@ -13,7 +13,7 @@ angular.module('appApp')
 .factory('TareasResourse', function ($resource) {
   var factory = {
 
-   iniciar: $resource('_server'+'/login',{},{
+   iniciar: $resource(_server+'/login',{},{
       sesion: {method: 'POST', params:{
         username:'@username',
         password:'@password'}
